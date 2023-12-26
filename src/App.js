17cad1a309d71c884be342c4dd8d710823207af2
Thrisha <thrishakannan25@gmail.com>
2023-12-26 23:41:18 +0530

    line added in app.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ function App() {
   return (
     <div className="App">
      <h1 className="text-red-500" >Hello World</h1>
+     <h1 className="text-red-500" >Hello friends</h1>
     </div>
   );
 }
